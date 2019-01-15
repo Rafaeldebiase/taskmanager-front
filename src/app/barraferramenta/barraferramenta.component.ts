d0e619edab483f71b04f90658fc29ce5eaf428c3
Rafael de Biase <rafaelvitaldebiase@gmail.com>
2019-01-15 02:19:51 -0200

    cria barra de ferramenta

---
+++ src/app/barraferramenta/barraferramenta.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tm-barraferramenta',
+  templateUrl: './barraferramenta.component.html',
+  styleUrls: ['./barraferramenta.component.scss']
+})
+export class BarraferramentaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
