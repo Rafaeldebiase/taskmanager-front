773b988d6e37ee96fd3eaa4b4ca44730db1db157
Rafael de Biase <rafaelvitaldebiase@gmail.com>
2019-01-15 02:20:25 -0200

    altera componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AreaTrabalhoComponent } from './area-trabalho/area-trabalho.component';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { MatToolbarModule } from '@angular/material';
 import { LoginComponent } from './login/login.component';
+import { BarraferramentaComponent } from './barraferramenta/barraferramenta.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AreaTrabalhoComponent,
-    LoginComponent
+    LoginComponent,
+    BarraferramentaComponent
   ],
   imports: [
     BrowserModule,
